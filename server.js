271788c9c3d8741cb535fcb43ef8415bfb7c0348
whoiswilcox <whoiswilcox@gmail.com>
2017-05-10 11:25:16 +1000

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var handleRequest = function(request, response) {
-  response.writeHead(200);
-  response.end("Hello Fifth Domain!");
-}
-var www = http.createServer(handleRequest);
-www.listen(8080);
